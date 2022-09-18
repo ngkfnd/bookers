8c594dd7c840d13f74f05c243ac24089bef01911
mako <110542097+ngkfnd@users.noreply.github.com>
2022-09-18 06:39:19 +0000

    bookers finish

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,44 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to root_path, success: '登録ができました'
+    else
+      flash.now[:denger] = "登録に失敗しました"
+      render :new
+    end
+  end
+
+  def index
+    @book = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:name, :email, :password, :password_confirmation)
+  end
+end
