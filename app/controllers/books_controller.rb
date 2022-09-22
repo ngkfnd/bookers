923081237c2e2166dad36bbef6dbe1bbc9bd9351
mako <110542097+ngkfnd@users.noreply.github.com>
2022-09-22 14:52:46 +0000

    bookers finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,13 +6,13 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      flash[:notice] = "Book was succesfully updated."
+      flash[:notice] = "Book was successfully created."
 
-      redirect_to "/booka/#{@book.id}"
+      redirect_to book_path(@book)
 
     else
       @books = Book.all
-      render "index"
+      render :index
     end
   end
 
@@ -30,15 +30,20 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully updated."
-    redirect_to "/books/#{book.id}"
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   post 'books' => 'books#create'
   get 'books' => 'books#index'
   get 'books/:id' => 'books#show'
-
+  root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
